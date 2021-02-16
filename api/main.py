7300d6d69073b461d93a4fe67e83658057cd8f0b
Christian Correa <garcimbo@outlook.com>
2021-02-16 15:44:23 -0500

    testing

--- api/main.py
+++ api/main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def index():
 
     if request.method == 'POST':
-        vals = request.form.getlist('daily_calories')
+        vals = request.form.getlist('numberx')
         val1 = float(vals[0])
         val2 = float(vals[1])
         result = val1 + val2
@@ -17,8 +17,8 @@ def index():
 
     return render_template_string('''
 <form action="/" method="POST">
-<p>Input 1: <input type="number" name="daily_calories" value="{{ val1 }}"></p>
-<p>Input 2: <input type="number" name="daily_calories" value="{{ val2 }}"></p>
+<p>Input 1: <input type="number" name="numberx" value="{{ val1 }}"></p>
+<p>Input 2: <input type="number" name="numberx" value="{{ val2 }}"></p>
 <input type="submit" value="Calculate">
 <p>Result: <input type="number" name="result_dc" value="{{ result }}"></p>
 </form>
